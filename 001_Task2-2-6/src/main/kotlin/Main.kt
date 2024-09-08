f67639b161cd9e2d2ce46b825da6415e339f62d5
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-09-09 03:07:39 +0700

    Add task 2.2.6 model

--- 001_Task2-2-6/src/main/kotlin/Main.kt
+++ 001_Task2-2-6/src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 package and.signal
 
-fun main() {
-    println("Hello World!")
-}
+data class Acc(val sum: Double, val lastTerm: Double)
+
+fun approxSin(x: Double, termsCount: Int) = (1..<termsCount).fold(Acc(x, x)) { acc, i ->
+    (acc.lastTerm * -1 * x * x / 2 * i / (2 * i + 1)).let { newTerm -> Acc(acc.sum + newTerm, newTerm) }
+}.sum
+
+const val arg = 0.1
+val termCounts = listOf(1, 3, 7)
+
+fun main() = termCounts.forEach { println("sin($arg) approx with $it term ${approxSin(arg, it)}") }
