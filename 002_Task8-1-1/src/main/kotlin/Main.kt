359f517e03f72366e43f551a1ef951e45300caca
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-09-10 16:03:20 +0700

    Fix Simpson

--- 002_Task8-1-1/src/main/kotlin/Main.kt
+++ 002_Task8-1-1/src/main/kotlin/Main.kt
@@ -2,34 +2,28 @@ package and.signal
 
 import kotlin.math.sqrt
 
-const val discretionFrequency = 0.001
-fun Double.discreteNext() = this + discretionFrequency
-fun Double.discretePrev() = this - discretionFrequency
+const val discretionFrequency = 0.01
+const val shiftFromBadBorder = 1e-6
 
 fun internalFunc(x: Double, y: Double, z: Double) = (x + y + z) / sqrt(2 * x * x + 4 * y * y + 5 * z * z)
+
+fun Double.discreteNext() = this + discretionFrequency
+fun Double.shift() = this + shiftFromBadBorder
 
 fun splitRange(from: Double, to: Double) = generateSequence(Pair(from, from.discreteNext())) { (_, right) ->
     if (right.discreteNext() > to) null else Pair(right, right.discreteNext())
 }
 
-//fun evalBlock(a: Double, b: Double, f: (Double) -> Double) = (b - a) * f((a + b) / 2)
-fun evalBlock(a: Double, b: Double, f: (Double) -> Double) = ((b - a) / 6) * (f(a) + f((a + b) / 2) + f(b))
+fun evalBlock(a: Double, b: Double, f: (Double) -> Double) = ((b - a) / 6) * (f(a) + 4 * f((a + b) / 2) + f(b))
 
 fun integrate(from: Double, to: Double, func: (Double) -> Double) = when {
     to < from -> throw IllegalArgumentException("Backward integration is unsupported")
-
-    from < to -> splitRange(from.discreteNext(), to.discretePrev()).sumOf { (left, right) ->
-        val evaluation = evalBlock(left, right, func)
-        if (evaluation.isNaN() || evaluation.isInfinite())
-            println("[$left, $right]: $evaluation")
-        evaluation.takeUnless { (it.isNaN() || it.isInfinite()) } ?: 0.0
-    }
-
+    from < to -> splitRange(from, to).sumOf { (left, right) -> evalBlock(left, right, func) }
     else -> 0.0
 }
 
-fun main() = integrate(0.0, 1.0) { x ->
-    integrate(0.0, sqrt(1 - x * x)) { y ->
-        integrate(0.0, sqrt(1 - x * x - y * y)) { z -> internalFunc(x, y, z) }
+fun main() = integrate(0.0.shift(), 1.0) { x ->
+    integrate(0.0.shift(), sqrt(1 - x * x)) { y ->
+        integrate(0.0.shift(), sqrt(1 - x * x - y * y)) { z -> internalFunc(x, y, z) }
     }
 }.let { println(it) }
