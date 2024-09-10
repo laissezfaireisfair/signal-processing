85ccf2f6a7522d475ee9fdd0bd9d5b0e60288b8f
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-09-10 15:42:08 +0700

    Use Simpson's Rule

--- 002_Task8-1-1/src/main/kotlin/Main.kt
+++ 002_Task8-1-1/src/main/kotlin/Main.kt
@@ -2,8 +2,9 @@ package and.signal
 
 import kotlin.math.sqrt
 
-const val discretionFrequency = 0.01
+const val discretionFrequency = 0.001
 fun Double.discreteNext() = this + discretionFrequency
+fun Double.discretePrev() = this - discretionFrequency
 
 fun internalFunc(x: Double, y: Double, z: Double) = (x + y + z) / sqrt(2 * x * x + 4 * y * y + 5 * z * z)
 
@@ -11,11 +12,19 @@ fun splitRange(from: Double, to: Double) = generateSequence(Pair(from, from.disc
     if (right.discreteNext() > to) null else Pair(right, right.discreteNext())
 }
 
-fun evalBlock(from: Double, to: Double, func: (Double) -> Double) = (to - from) * func((from + to) / 2)
+//fun evalBlock(a: Double, b: Double, f: (Double) -> Double) = (b - a) * f((a + b) / 2)
+fun evalBlock(a: Double, b: Double, f: (Double) -> Double) = ((b - a) / 6) * (f(a) + f((a + b) / 2) + f(b))
 
 fun integrate(from: Double, to: Double, func: (Double) -> Double) = when {
     to < from -> throw IllegalArgumentException("Backward integration is unsupported")
-    from < to -> splitRange(from, to).sumOf { (left, right) -> evalBlock(left, right, func) }
+
+    from < to -> splitRange(from.discreteNext(), to.discretePrev()).sumOf { (left, right) ->
+        val evaluation = evalBlock(left, right, func)
+        if (evaluation.isNaN() || evaluation.isInfinite())
+            println("[$left, $right]: $evaluation")
+        evaluation.takeUnless { (it.isNaN() || it.isInfinite()) } ?: 0.0
+    }
+
     else -> 0.0
 }
 
