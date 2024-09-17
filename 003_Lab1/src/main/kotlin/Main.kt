57dd4e9bf0cbf952342d99b785ddf53dff576c7d
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-09-17 14:05:19 +0700

    Add computation of Fourier coefficients

--- 003_Lab1/src/main/kotlin/Main.kt
+++ 003_Lab1/src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 package and.signal
 
+import and.signal.fourier.Fourier.Companion.toFourier
 import and.signal.integrate.discretionFrequency
 import kotlin.math.floor
 import org.math.plot.Plot2DPanel
@@ -7,6 +8,8 @@ import javax.swing.JFrame
 
 const val from: Double = -4.0
 const val to: Double = 4.0
+const val terms = 5
+const val frequency = 100.0
 
 fun x(t: Double) = when {
     floor(t).toInt().rem(2).let { it == 1 || it == -1 } -> 2.0
@@ -15,11 +18,13 @@ fun x(t: Double) = when {
 
 val args = generateSequence(from) { if (it < to) it + discretionFrequency else null }.toList().toDoubleArray()
 
-fun main() = Plot2DPanel().apply {
-    addLinePlot("Signal", args, args.map { x(it) }.toDoubleArray())
-}.let {
-    with(JFrame("Plots")) {
-        contentPane = it
-        isVisible = true
+fun main() = println(::x.toFourier(terms, from, 1.0, frequency)).also {
+    Plot2DPanel().apply {
+        addLinePlot("Signal", args, args.map { x(it) }.toDoubleArray())
+    }.let {
+        with(JFrame("Plots")) {
+            contentPane = it
+            isVisible = true
+        }
     }
 }
--- 003_Lab1/src/main/kotlin/fourier/Fourier.kt
+++ 003_Lab1/src/main/kotlin/fourier/Fourier.kt
@@ -1,31 +1,37 @@
 package and.signal.fourier
 
 import and.signal.integrate.integrate
+import kotlin.math.cos
+import kotlin.math.sin
 
 data class Fourier(val a0: Double, val aAndBs: List<Pair<Double, Double>>) {
     companion object {
         private fun ((Double) -> Double).getA0(from: Double, period: Double) =
             2 / period * integrate(from, from + period, this)
 
-        private fun ((Double) -> Double).getAAndBs(count: Int, from: Double, period: Double) =
-            (1..count).map { Pair(TODO(), TODO()) }
+        private fun ((Double) -> Double).getAAndBs(count: Int, from: Double, period: Double, frequency: Double) =
+            (1..count).map { n ->
+                Pair(2 / period * integrate(from, from + period) { this(it) * cos(n * frequency * it) },
+                    2 / period * integrate(from, from + period) { this(it) * sin(n * frequency * it) })
+            }
 
-        fun ((Double) -> Double).toFourier(count: Int, from: Double, period: Double): Fourier = when {
-            count < 0 -> throw IllegalArgumentException(
-                "Count of fourier coefficients shouldn't be negative (has $count)"
-            )
+        fun ((Double) -> Double).toFourier(terms: Int, from: Double, period: Double, frequency: Double): Fourier =
+            when {
+                terms < 0 -> throw IllegalArgumentException(
+                    "Count of fourier coefficients shouldn't be negative (has $terms)"
+                )
 
-            period <= 0.0 -> throw IllegalArgumentException("Period should be positive (has $period")
+                period <= 0.0 -> throw IllegalArgumentException("Period should be positive (has $period")
 
-            else -> Fourier(a0 = this.getA0(from, period), aAndBs = this.getAAndBs(count, from, period))
-        }
+                else -> Fourier(a0 = this.getA0(from, period), aAndBs = this.getAAndBs(terms, from, period, frequency))
+            }
     }
 
-    val maxTerms = aAndBs.size
+    val terms = aAndBs.size
 
     fun approximate(arg: Double, termsRequired: Int? = null): Double = when {
-        termsRequired != null && maxTerms < termsRequired -> throw IllegalArgumentException(
-            "Required more terms ($termsRequired) than we have coefficients ($maxTerms)"
+        termsRequired != null && terms < termsRequired -> throw IllegalArgumentException(
+            "Required more terms ($termsRequired) than we have coefficients ($terms)"
         )
 
         else -> TODO()
