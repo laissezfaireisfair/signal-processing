0c5eff5d55ab4779dd929f3bf8f29c2717001934
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-09-18 02:20:42 +0700

    Fix Fourier

--- 003_Lab1/src/main/kotlin/Main.kt
+++ 003_Lab1/src/main/kotlin/Main.kt
@@ -1,17 +1,16 @@
 package and.signal
 
-import and.signal.fourier.Fourier.Companion.toFourier
+import and.signal.fourier.approximate
+import and.signal.fourier.toFourier
 import and.signal.integrate.discretionFrequency
 import kotlin.math.floor
 import org.math.plot.Plot2DPanel
 import java.awt.Color
-import java.awt.Dimension
 import javax.swing.JFrame
-import kotlin.math.PI
 
 const val from: Double = -4.0
 const val to: Double = 4.0
-const val terms = 5
+const val terms = 20
 const val period = 2.0
 
 fun x(t: Double) = when {
@@ -21,15 +20,16 @@ fun x(t: Double) = when {
 
 val args = generateSequence(from) { if (it < to) it + discretionFrequency else null }.toList().toDoubleArray()
 
-fun main() = ::x.toFourier(terms, 0.0, period).let { fourier ->
-    println(fourier)
-    Plot2DPanel().apply {
+fun main() {
+    val fourier = toFourier(terms, period) { x(it) }
+
+    val panel = Plot2DPanel().apply {
         addLinePlot("Signal", Color.BLUE, args, args.map { x(it) }.toDoubleArray())
-        addLinePlot("Approximation", Color.YELLOW, args, args.map { fourier.approximate(it) }.toDoubleArray())
-    }.let {
-        with(JFrame("Plots")) {
-            contentPane = it
-            isVisible = true
-        }
+        addLinePlot("Approximation", Color.ORANGE, args, args.map { fourier.approximate(it) }.toDoubleArray())
+    }
+
+    with(JFrame("Plots")) {
+        contentPane = panel
+        isVisible = true
     }
 }
--- 003_Lab1/src/main/kotlin/fourier/Fourier.kt
+++ 003_Lab1/src/main/kotlin/fourier/Fourier.kt
@@ -5,36 +5,38 @@ import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.sin
 
-data class Fourier(val a0: Double, val period: Double, val aAndBs: List<Pair<Double, Double>>) {
-    companion object {
-        private fun toW(period: Double) = 2 * PI / period
+data class Fourier(val a0: Double, val period: Double, val aAndBs: List<Pair<Double, Double>>)
 
-        fun ((Double) -> Double).toFourier(terms: Int, from: Double, period: Double): Fourier = when {
-            terms < 0 -> throw IllegalArgumentException(
-                "Count of fourier coefficients shouldn't be negative (has $terms)"
-            )
+fun toFourier(terms: Int, period: Double, f: (Double) -> Double): Fourier = when {
+    terms < 0 -> throw IllegalArgumentException(
+        "Count of fourier coefficients shouldn't be negative (has $terms)"
+    )
 
-            period <= 0.0 -> throw IllegalArgumentException("Period should be positive (has $period")
+    period <= 0.0 -> throw IllegalArgumentException("Period should be positive (has $period")
 
-            else -> Fourier(
-                a0 = 2 / period * integrate(from, from + period, this),
-                period = period,
-                aAndBs = (1..terms).map { n ->
-                    Pair(2 / period * integrate(from, from + period) { this(it) * cos(n * toW(period) * it) },
-                        2 / period * integrate(from, from + period) { this(it) * sin(n * toW(period) * it) })
-                })
-        }
+    else -> {
+        val w = (2 * PI / period)
+        val from = -period / 2
+        val to = period / 2
+
+        Fourier(a0 = 2 / period * integrate(from, to, f), period = period, aAndBs = (1..terms).map { n ->
+            Pair(2 / period * integrate(from, to) { f(it) * cos(n * w * it) },
+                2 / period * integrate(from, to) { f(it) * sin(n * w * it) })
+        })
     }
+}
 
-    fun approximate(arg: Double, termsRequired: Int? = null): Double = when {
-        termsRequired != null && aAndBs.size < termsRequired -> throw IllegalArgumentException(
-            "Required more terms ($termsRequired) than we have coefficients (${aAndBs.size})"
-        )
+fun Fourier.approximate(arg: Double, termsRequired: Int? = null): Double = when {
+    termsRequired != null && aAndBs.size < termsRequired -> throw IllegalArgumentException(
+        "Required more terms ($termsRequired) than we have coefficients (${aAndBs.size})"
+    )
 
-        else -> (termsRequired ?: aAndBs.size).let { terms ->
-            a0 / 2 + (1..terms).sumOf { n ->
-                aAndBs[n - 1].let { (an, bn) -> an * cos(n * toW(period) * arg) + bn * cos(n * toW(period) * arg) }
-            }
+    else -> {
+        val w = (2 * PI / period)
+        val terms = termsRequired ?: aAndBs.size
+
+        a0 / 2 + (1..terms).sumOf { n ->
+            aAndBs[n - 1].let { (an, bn) -> an * cos(n * w * arg) + bn * sin(n * w * arg) }
         }
     }
 }
