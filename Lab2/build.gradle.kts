4cda5f248414eb9092734dcb61ab0eae20eaaad5
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-10-20 20:37:49 +0700

    Add lab 2 draft

---
+++ Lab2/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+}
+
+group = "and.signal"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.kotlin.link")
+}
+
+dependencies {
+    api("space.kscience:kmath-core:0.4.0")
+    api("space.kscience:kmath-commons:0.4.0")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
---
+++ Lab2/src/main/kotlin/fourier/dftSlow.kt
@@ -0,0 +1,5 @@
+package fourier
+
+import space.kscience.kmath.complex.Complex
+
+fun dftSlow(values: List<Double>):Complex = TODO("Not yet implemented")
