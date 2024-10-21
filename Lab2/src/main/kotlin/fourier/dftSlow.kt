118e62bd714fabe7490e1b3ff577ce70c35cb7a7
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-10-21 10:13:07 +0700

    WIP on dft slow

--- Lab2/src/main/kotlin/fourier/dftSlow.kt
+++ Lab2/src/main/kotlin/fourier/dftSlow.kt
@@ -1,5 +1,15 @@
 package fourier
 
 import space.kscience.kmath.complex.Complex
+import space.kscience.kmath.complex.toComplex
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 
-fun dftSlow(values: List<Double>):Complex = TODO("Not yet implemented")
+operator fun Complex.times(other: Complex): Complex = TODO("Not yet implemented")
+
+fun Iterable<Complex>.sum(): Complex = TODO("Not yet implemented")
+
+fun dftSlow(values: List<Double>): Complex = values.mapIndexed { i, e ->
+    e.toComplex() * Complex(re = cos(2 * PI * i / values.size), im = -sin(2 * PI * i / values.size))
+}.sum()
